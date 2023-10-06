906f908c08bd00381e86f20d23da450c8e747ee6
Vaishnavi6682 <145091648+Vaishnavi6682@users.noreply.github.com>
2023-10-06 09:08:06 +0530

    Add files via upload

---
+++ Challenge 3 unit 3/main (5).py
@@ -0,0 +1,37 @@
+#Let implement a function sort_students
+#with each student attributes of name,rollnumber and cgpa
+#And sort The students using CGPA in Descending Order
+#Check the function with different Input
+
+class student:
+
+  def __init__(self,name,roll_number,cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # let sort the list of students in descending order of cgpa
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  return sorted_students
+
+# lets check the example for the code :
+students = [
+  student("HARSHA","CSC122",9.3),
+  student("DEEPA","CSC116",6.3),
+  student("HEMANTHIN","CSC123",9.1),
+  student("HARSHITHA","CSC124",6.9)
+]
+
+sorted_students = sort_students(students)
+
+#lets print the sorted students
+print("List of Sorted Students in descending order \n")
+
+for student in sorted_students:
+  print("Name: {},Roll Number: {},CGPA: {}".format(student.name,
+                                                  student.roll_number,
+                                                  student.cgpa))
